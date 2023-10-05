661837256dc4ce6346013c445e6999f6e8508004
Bee <amy.bee.vanzyl@gmail.com>
2023-10-05 17:32:29 +0100

    click text to get modal

--- frontend/public/script/dashboard.js
+++ frontend/public/script/dashboard.js
@@ -1,9 +1,16 @@
 const entryList = document.querySelector("#entries ul")
+const modal = document.getElementById("full-entry")
+const exitModal = document.getElementById("close")
 console.log(entryList)
 console.log(entryList.lastChild)
 
 entryList.lastElementChild.addEventListener("click", getModal)
+exitModal.addEventListener("click", closeModal)
+// window.addEventListener("click", closeModal)
 
 function getModal() {
-    alert("a")
+    modal.style.display = "block";
+}
+function closeModal() {
+    modal.style.display = "none";
 }
